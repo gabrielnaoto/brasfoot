2826fecd786fb9a0893747aa330082a710ec68f9
Luma Hoscher <luhoscher@gmail.com>
2016-11-03 22:40:10 -0200

    19 - times em regras

--- app/FutManDDM/app/src/main/java/com/example/tonied/futmanddm/modelo/entidade/Regras.java
+++ app/FutManDDM/app/src/main/java/com/example/tonied/futmanddm/modelo/entidade/Regras.java
@@ -1,4 +1,4 @@
-package br.udesc.ddm.brasfoot.modelo.entidade;
+package com.example.tonied.futmanddm.modelo.entidade;
 
 public class Regras {
 
@@ -23,4 +23,92 @@ public class Regras {
         return placar;
     }
 
+    public String[][] getCalendario (int semana){
+        switch (semana){
+            case 1:{
+                return {{"Real Madrid", "Barcelona"},
+                        {"Atlético Madrid", "Bayern"},
+                        {"Juventus", "Manchester United"},
+                        {"Arsenal", "Paris Saint-Germain"}};
+            }
+            case 2:{
+                return {{"Bayern","Real Madrid"},
+                        {"Barcelona","Atlético Madrid"},
+                        {"Paris Saint-Germain","Juventus"},
+                        {"Manchester United","Arsenal}};
+            }
+            case 3:{
+                return {{"Real Madrid","Atlético Madrid"},
+                        {"Bayern","Barcelona"},
+                        {"Arsenal","Juventus"},
+                        {"Manchester United","Paris Saint-Germain"}};
+            }
+            case 4:{
+                return {{"Juventus","Real Madrid"},
+                        {"Barcelona","Manchester United"},
+                        {"Paris Saint-Germain","Bayern"},
+                        {"Atlético Madrid","Arsenal"}};
+            }
+            case 5:{
+                return {{"Real Madrid","Manchester United"},
+                        {"Barcelona","Juventus"},
+                        {"Bayern","Arsenal"},
+                        {"Atlético Madrid","Paris Saint-Germain"}};
+            }
+            case 6:{
+                return {{"Paris Saint-Germain","Real Madrid"},
+                        {"Arsenal","Barcelona"},
+                        {"Juventus","Bayern"},
+                        {"Manchester United","Atlético Madrid"}};
+            }
+            case 7:{
+                return {{"Real Madrid","Arsenal"},
+                        {"Barcelona","Paris Saint-Germain"},
+                        {"Bayern","Manchester United"},
+                        {"Atlético Madrid","Juventus"}};
+            }
+            case 8:{
+                return {{"Barcelona","Real Madrid"},
+                        {"Bayern","Atlético Madrid"},
+                        {"Manchester United","Juventus"},
+                        {"Paris Saint-Germain","Arsenal"}};
+            }
+            case 9:{
+                return {{"Real Madrid","Bayern"},
+                        {"Atlético Madrid","Barcelona"},
+                        {"Juventus","Paris Saint-Germain"},
+                        {"Arsenal","Manchester United"}};
+            }
+            case 10:{
+                return {{"Atlético Madrid","Real Madrid"},
+                        {"Barcelona","Bayern"},
+                        {"Juventus","Arsenal"},
+                        {"Paris Saint-Germain","Manchester United"}};
+            }
+            case 11:{
+                return {{"Real Madrid","Juventus"},
+                        {"Manchester United","Barcelona"},
+                        {"Bayern","Paris Saint-Germain"},
+                        {"Arsenal","Atlético Madrid"}};
+            }
+            case 12:{
+                return {{"Manchester United","Real Madrid"},
+                        {"Juventus","Barcelona"},
+                        {"Arsenal","Bayern"},
+                        {"Paris Saint-Germain","Atlético Madrid"}};
+            }
+            case 13:{
+                return {{"Real Madrid","Paris Saint-Germain"},
+                        {"Barcelona","Arsenal"},
+                        {"Bayern","Juventus"},
+                        {"Atlético Madrid","Manchester United"}};
+            }
+            case 14:{
+                return {{"Arsenal","Real Madrid"},
+                        {"Paris Saint-Germain","Barcelona"},
+                        {"Manchester United","Bayern"},
+                        {"Juventus","Atlético Madrid"}};
+            }
+        }
+    }
 }
