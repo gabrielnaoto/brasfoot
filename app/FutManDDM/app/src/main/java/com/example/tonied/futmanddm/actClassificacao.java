d53b7375a637bdc00b333f287d982ecea1dcdd62
Luma Hoscher <luhoscher@gmail.com>
2016-11-09 02:00:03 -0200

    33 - activity classificacao

--- app/FutManDDM/app/src/main/java/com/example/tonied/futmanddm/actClassificacao.java
+++ app/FutManDDM/app/src/main/java/com/example/tonied/futmanddm/actClassificacao.java
@@ -1,5 +1,6 @@
 package com.example.tonied.futmanddm;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -14,8 +15,17 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import com.example.tonied.futmanddm.modelo.dao.core.TimeDAO;
+import com.example.tonied.futmanddm.modelo.dao.sqlite.SQLTimeDAO;
+import com.example.tonied.futmanddm.modelo.entidade.Regras;
+import com.example.tonied.futmanddm.modelo.entidade.Time;
+
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class actClassificacao extends AppCompatActivity {
     private Button btVoltar;
@@ -44,30 +54,11 @@ public class actClassificacao extends AppCompatActivity {
     private TextView nome06;
     private TextView nome07;
     private TextView nome08;
+    private Map<String, View> views;
+    private TimeDAO timedao;
+    private Time t;
 
-    int[] times = {
-            R.drawable.earsenal,
-            R.drawable.eatlmadrid,
-            R.drawable.ebarcelona,
-            R.drawable.ebayern,
-            R.drawable.ejuventus,
-            R.drawable.emanunited,
-            R.drawable.epsg,
-            R.drawable.erealm
-    };
-
-    int[] pontos ={12, 11, 10, 9, 8, 7, 6, 5};
-
-    String[] nomeTime = {
-            "Arsenal",
-            "Atl. Madrid",
-            "Barcelona",
-            "Bayern Munique",
-            "Juventus",
-            "Manchester Utd",
-            "Paris SG",
-            "Real Madrid"
-    };
+    Time[] times = new Time[8];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,38 +68,49 @@ public class actClassificacao extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
-        btVoltar = (Button)findViewById(R.id.btVoltar);
-        tabela = (TableLayout)findViewById(R.id.tabela);
-        escudo01 = (ImageView)findViewById(R.id.escudo01);
-        escudo02 = (ImageView)findViewById(R.id.escudo02);
-        escudo03 = (ImageView)findViewById(R.id.escudo03);
-        escudo04 = (ImageView)findViewById(R.id.escudo04);
-        escudo05 = (ImageView)findViewById(R.id.escudo05);
-        escudo06 = (ImageView)findViewById(R.id.escudo06);
-        escudo07 = (ImageView)findViewById(R.id.escudo07);
-        escudo08 = (ImageView)findViewById(R.id.escudo08);
-        pontos01 = (TextView)findViewById(R.id.pontos01);
-        pontos02 = (TextView)findViewById(R.id.pontos02);
-        pontos03 = (TextView)findViewById(R.id.pontos03);
-        pontos04 = (TextView)findViewById(R.id.pontos04);
-        pontos05 = (TextView)findViewById(R.id.pontos05);
-        pontos06 = (TextView)findViewById(R.id.pontos06);
-        pontos07 = (TextView)findViewById(R.id.pontos07);
-        pontos08 = (TextView)findViewById(R.id.pontos08);
-        nome01 = (TextView)findViewById(R.id.nome01);
-        nome02 = (TextView)findViewById(R.id.nome02);
-        nome03 = (TextView)findViewById(R.id.nome03);
-        nome04 = (TextView)findViewById(R.id.nome04);
-        nome05 = (TextView)findViewById(R.id.nome05);
-        nome06 = (TextView)findViewById(R.id.nome06);
-        nome07 = (TextView)findViewById(R.id.nome07);
-        nome08 = (TextView)findViewById(R.id.nome08);
+        SQLiteDatabase db = openOrCreateDatabase("brasfoot", MODE_PRIVATE, null);
+        timedao = new SQLTimeDAO(db);
+
+        btVoltar = (Button) findViewById(R.id.btVoltar);
+        tabela = (TableLayout) findViewById(R.id.tabela);
+        escudo01 = (ImageView) findViewById(R.id.escudo01);
+        escudo02 = (ImageView) findViewById(R.id.escudo02);
+        escudo03 = (ImageView) findViewById(R.id.escudo03);
+        escudo04 = (ImageView) findViewById(R.id.escudo04);
+        escudo05 = (ImageView) findViewById(R.id.escudo05);
+        escudo06 = (ImageView) findViewById(R.id.escudo06);
+        escudo07 = (ImageView) findViewById(R.id.escudo07);
+        escudo08 = (ImageView) findViewById(R.id.escudo08);
+        pontos01 = (TextView) findViewById(R.id.pontos01);
+        pontos02 = (TextView) findViewById(R.id.pontos02);
+        pontos03 = (TextView) findViewById(R.id.pontos03);
+        pontos04 = (TextView) findViewById(R.id.pontos04);
+        pontos05 = (TextView) findViewById(R.id.pontos05);
+        pontos06 = (TextView) findViewById(R.id.pontos06);
+        pontos07 = (TextView) findViewById(R.id.pontos07);
+        pontos08 = (TextView) findViewById(R.id.pontos08);
+        nome01 = (TextView) findViewById(R.id.nome01);
+        nome02 = (TextView) findViewById(R.id.nome02);
+        nome03 = (TextView) findViewById(R.id.nome03);
+        nome04 = (TextView) findViewById(R.id.nome04);
+        nome05 = (TextView) findViewById(R.id.nome05);
+        nome06 = (TextView) findViewById(R.id.nome06);
+        nome07 = (TextView) findViewById(R.id.nome07);
+        nome08 = (TextView) findViewById(R.id.nome08);
+
+        List<Time> timess = timedao.listar();
+
+        int ti = 0;
+        for (Time t : timess) {
+            times[ti++] = t;
+        }
 
         criaDadosTabela();
         btVoltarClick();
     }
 
     private void btVoltarClick() {
+
         btVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -118,29 +120,29 @@ public class actClassificacao extends AppCompatActivity {
     }
 
     public void criaDadosTabela() {
-        escudo01.setImageResource(times[0]);
-        escudo02.setImageResource(times[1]);
-        escudo03.setImageResource(times[2]);
-        escudo04.setImageResource(times[3]);
-        escudo05.setImageResource(times[4]);
-        escudo06.setImageResource(times[5]);
-        escudo07.setImageResource(times[6]);
-        escudo08.setImageResource(times[7]);
-        pontos01.setText(pontos[0]+"");
-        pontos02.setText(pontos[1]+"");
-        pontos03.setText(pontos[2]+"");
-        pontos04.setText(pontos[3]+"");
-        pontos05.setText(pontos[4]+"");
-        pontos06.setText(pontos[5]+"");
-        pontos07.setText(pontos[6]+"");
-        pontos08.setText(pontos[7]+"");
-        nome01.setText(nomeTime[0]);
-        nome02.setText(nomeTime[1]);
-        nome03.setText(nomeTime[2]);
-        nome04.setText(nomeTime[3]);
-        nome05.setText(nomeTime[4]);
-        nome06.setText(nomeTime[5]);
-        nome07.setText(nomeTime[6]);
-        nome08.setText(nomeTime[7]);
+        escudo01.setImageResource(Regras.times[times[0].getTimeid()]);
+        escudo02.setImageResource(Regras.times[times[1].getTimeid()]);
+        escudo03.setImageResource(Regras.times[times[2].getTimeid()]);
+        escudo04.setImageResource(Regras.times[times[3].getTimeid()]);
+        escudo05.setImageResource(Regras.times[times[4].getTimeid()]);
+        escudo06.setImageResource(Regras.times[times[5].getTimeid()]);
+        escudo07.setImageResource(Regras.times[times[6].getTimeid()]);
+        escudo08.setImageResource(Regras.times[times[7].getTimeid()]);
+        pontos01.setText(times[0].getPontos() + "");
+        pontos02.setText(times[1].getPontos() + "");
+        pontos03.setText(times[2].getPontos() + "");
+        pontos04.setText(times[3].getPontos() + "");
+        pontos05.setText(times[4].getPontos() + "");
+        pontos06.setText(times[5].getPontos() + "");
+        pontos07.setText(times[6].getPontos() + "");
+        pontos08.setText(times[7].getPontos() + "");
+        nome01.setText(times[0].getNome());
+        nome02.setText(times[1].getNome());
+        nome03.setText(times[2].getNome());
+        nome04.setText(times[3].getNome());
+        nome05.setText(times[4].getNome());
+        nome06.setText(times[5].getNome());
+        nome07.setText(times[6].getNome());
+        nome08.setText(times[7].getNome());
     }
 }
